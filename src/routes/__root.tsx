2d63046fb33fa282f0400e10b3d4408577490772
Viet Hung <viethung01234@gmail.com>
2025-08-09 18:23:47 +0700

    feat: integrate zustand for state management and create initial store configuration

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Outlet, createRootRoute } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 import { useTranslation } from 'react-i18next';
+import useAppStore from '@/configs/store.config';
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -9,9 +10,11 @@ export const Route = createRootRoute({
 
 function RootComponent() {
   const { t } = useTranslation();
+  const store = useAppStore();
   return (
     <React.Fragment>
       <div>{t('Hi')} "__root"!</div>
+      <div>Store test value: {store.test}</div>
       <Outlet />
       <TanStackRouterDevtools />
     </React.Fragment>
